fa7285113016ed89d3eaa74924dc913f66080992
Ryan Block <rblock@gmail.com>
2022-02-09 14:34:58 -0800

    Expand reserved env var list

--- src/_is-reserved.js
+++ src/_is-reserved.js
@@ -1,4 +1,11 @@
 module.exports = function isReserved (key) {
-  let reserved = [ 'ARC_ENV', 'ARC_APP_NAME' ]
+  let reserved = [
+    'ARC_APP_NAME',
+    'ARC_DISABLE_ENV_VARS',
+    'ARC_ENV',
+    'ARC_ROLE',
+    'ARC_SANDBOX',
+    'ARC_WSS_URL',
+  ]
   return reserved.includes(key)
 }
